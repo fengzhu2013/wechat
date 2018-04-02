fcea67d203a4fc07a010bb033a1a7c49420095f9
fengzhu2013 <359418894@qq.com>
2018-04-02 14:26:49 +0800

    完成了模块的规划
    完成了模块规划，准备引入swagger-php

---
+++ application/system/controller/Admin.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace app\system\controller;
+
+class Admin
+{
+    const LOGIN_EXPIRE_TIME = 21600;            //登陆有效期为6h，单位s
+
+    /**
+     * 是否登陆
+     * @var bool
+     */
+    private $isLogin = false;
+
+    /**
+     * 登陆身份类型,1-普通，2-超管
+     * @var
+     */
+    private $loginType;
+
+    /**
+     * 登陆信息
+     * @var array
+     */
+    private $loginInfo = [];
+
+    /**
+     * 登陆的身份信息是否拥有调用action的权限
+     * @var bool
+     */
+    private $isPower = false;
+
+    public function __construct()
+    {
+        //检验是否登陆，若登陆了，给loginInfo赋值
+        $this->checkIsLogin();
+        //如果没有登录,输出提示登录信息
+        if (!$this->isLogin) {
+
+        }
+
+        //登陆了，
+    }
+
+    /**
+     * 检验是否登陆
+     */
+    private function checkIsLogin()
+    {
+        //获得登陆信息
+        $this->loginInfo = $this->getLoginInfo();
+        //如果登陆信息为空
+        if (!count($this->loginInfo))
+            $this->isLogin = false;
+        //如果登陆了，检验登陆是否过期
+        if (!isset($this->loginInfo['loginTime']) || $this->loginInfo['loginTime'] + self::LOGIN_EXPIRE_TIME < time())
+            $this->isLogin = false;
+        $this->isLogin = true;
+    }
+
+    /**
+     * 获得登陆信息，如果不存在，返回空的数组
+     * @return array
+     */
+    private function getLoginInfo()
+    {
+        return [];
+    }
+
+    /**
+     * 注销，退出登录状态
+     */
+    public function logout()
+    {
+
+    }
+
+    /**
+     * 新建一个普通管理员
+     */
+    public function createAdmin()
+    {
+
+    }
+
+    /**
+     * 输出登陆信息及权限信息
+     */
+    public function toStringLoginInfo()
+    {
+
+    }
+
+    /**
+     * 修改管理员信息
+     */
+    public function modifyAdminInfo()
+    {
+
+    }
+
+    /**
+     * 获得管理员列表
+     */
+    public function getAdminList()
+    {
+
+    }
+
+    /**
+     * 获得管理员操作记录列表
+     */
+    public function getWriteLogList()
+    {
+
+    }
+
+
+
+}
---
+++ application/system/controller/Index.php
@@ -0,0 +1,114 @@
+<?php
+namespace App\system\controller;
+
+use EasyWeChat\Factory;
+class Index
+{
+
+    protected $app_id;
+
+    protected $secret;
+
+    protected $token;
+
+    protected $aes_key;
+
+    public function __construct()
+    {
+        //获得配置信息
+        $this->initSystem();
+    }
+
+    /**
+     * connect server of wechat
+     * only for wechat server
+     */
+    public function connectWeChat()
+    {
+        //配置信息
+        $config = [
+            'app_id'  => 'wx6974dcaaa1831a5d',
+            'secret'  => '44f163398c5bffbdffa2750cf4bff070',
+            'token'   => 'trueFly',
+            'aes_key' => 'I5nj4E8jUmjSsQRwaGnTx55LhwXelWWf8rOahKnuu5r',                    // EncodingAESKey，兼容与安全模式下请一定要填写！！！
+
+            'response_type' => 'array',
+
+            'log' => [
+                'level' => 'debug',
+                'file' => __DIR__ . '/wechat.log',
+            ],
+        ];
+
+        // 使用配置来初始化一个公众号应用实例。
+        $app = Factory::officialAccount($config);
+
+        //消息处理
+        $app->server->push(function ($message) {
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+        });
+
+        //执行服务端业务
+        $response = $app->server->serve();
+
+        // 将响应结果输出
+        $response->send();
+    }
+
+    /**
+     * 初始化配置信息
+     */
+    private function initSystem()
+    {
+        //获得数据库的信息
+        $info = [];
+    }
+
+    /**
+     * 系统生成管理员
+     */
+    public function createSystemAdmin()
+    {
+
+    }
+
+    /**
+     * 系统登录，需要写入登陆日志数据库
+     */
+    public function systemLogin()
+    {
+
+    }
+
+
+
+
+
+
+}
